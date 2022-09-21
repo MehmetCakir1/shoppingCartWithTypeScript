f4bcf24a0dd8ee2ede7855552dae907652895a20
MehmetCakir1 <mehmetcakir4227@gmail.com>
2022-09-21 20:00:43 +0300

    cart

--- src/components/CartItem.tsx
+++ src/components/CartItem.tsx
@@ -1,3 +1,6 @@
+import data from "../data/data.json"
+import { formatCurrency } from "../utils/formatCurrency"
+import {FaTimes} from "react-icons/fa"
 
 type CartItemProps = {
     id:number
@@ -5,8 +8,23 @@ type CartItemProps = {
 }
 
 const CartItem = ({id,quantity}: CartItemProps) => {
+
+  const item = data.find((item)=>item.id==id)
+  if(item ==null){
+    return null
+  }
+// console.log(item);
   return (
-    <div>CartItem</div>
+    <div>
+      <div className="flex px-3 mt-7">
+        <img src={item.imgUrl} alt={item.name} className="w-36" />
+        <p className="flex flex-col items-start justify-center font-bold px-3 w-[90px]"><span>{item.name} {quantity>1 && "x "+quantity}</span> <span>{formatCurrency(item.price)}</span></p>
+        <p className="flex justify-center items-center font-bold text-2xl px-2 text-teal-900 w-[125px]">{formatCurrency(item.price*quantity)}</p>
+        <div className="flex items-center justify-center px-2 text-red-600">
+        <button><FaTimes/></button>
+        </div>
+      </div>
+    </div>
   )
 }
 
--- src/components/ShoppingCart.tsx
+++ src/components/ShoppingCart.tsx
@@ -8,21 +8,27 @@ type ShoppingCartProps = {
 
 const ShoppingCart = ({isOpen}: ShoppingCartProps) => {
     const {closeCart,cartItems}=useShoppingCartContext()
+    // console.log(cartItems);
 
   return (
-    <div>
-        <div>
-            <h1>CART</h1>
-            <button onClick={()=>closeCart}><FaTimes/></button>
-        </div>
-        <div>
-            {cartItems?.map((item)=>{
-                return(
-                    <CartItem key={item.id} {...item}/>
-                )
-            })}
-        </div>
+<>
+{
+  isOpen &&
+    <div className='w-[26rem] h-screen bg-cyan-200 absolute right-0 top-0 transition-width 1s easy'>
+    <div className='flex justify-between items-center px-3 py-3 text-2xl font-bold'>
+        <h1>CART</h1>
+        <button onClick={closeCart} className="text-red-600"><FaTimes/></button>
     </div>
+    <div>
+        {cartItems?.map((item)=>{
+            return(
+                <CartItem key={item.id} {...item}/>
+            )
+        })}
+    </div>
+</div>
+}
+</>
   )
 }
 
